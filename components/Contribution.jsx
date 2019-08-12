c11ae84573004d9645a107b902bc7cc8b875a2d8
NightNineNeighbor <dud9203@gmail.com>
2019-08-12 20:58:13 +0900

    [수정] nick -> anne

--- components/Contribution.jsx
+++ components/Contribution.jsx
@@ -2,18 +2,18 @@ import React, { useEffect } from "react";
 import GitHubCalendar from "../lib/github-calendar";
 
 const Contribution = () => {
-  useEffect(() => {
-    GitHubCalendar(".github-container", "nick-615");
-  });
-  return (
-    <>
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/github-calendar@latest/dist/github-calendar-responsive.css"
-      />
-      <div className="github-container" />
-    </>
-  );
+    useEffect(() => {
+        GitHubCalendar(".github-container", "anne-higher");
+    });
+    return (
+        <>
+            <link
+                rel="stylesheet"
+                href="https://unpkg.com/github-calendar@latest/dist/github-calendar-responsive.css"
+            />
+            <div className="github-container" />
+        </>
+    );
 };
 
 export default Contribution;
